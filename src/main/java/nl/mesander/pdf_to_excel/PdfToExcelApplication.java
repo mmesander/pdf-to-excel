c95edd712e7458d13307c298954c2dcd12273001
Mark Mesander <mark_mesander@hotmail.com>
2025-01-08 17:37:51 +0100

    first commit

---
+++ src/main/java/nl/mesander/pdf_to_excel/PdfToExcelApplication.java
@@ -0,0 +1,13 @@
+package nl.mesander.pdf_to_excel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfToExcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfToExcelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/mesander/pdf_to_excel/PdfToExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.mesander.pdf_to_excel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfToExcelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
