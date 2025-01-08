e2fe3a3b76bdc58d020ac5f684c38e1b42b0c032
Mark Mesander <mark_mesander@hotmail.com>
2025-01-08 19:33:11 +0100

    add transfer methods and empty crud methods in service

--- src/main/java/nl/mesander/pdf_to_excel/services/InvoiceService.java
+++ src/main/java/nl/mesander/pdf_to_excel/services/InvoiceService.java
@@ -1,6 +1,13 @@
 package nl.mesander.pdf_to_excel.services;
 
+import nl.mesander.pdf_to_excel.dtos.input.InvoiceInputDto;
+import nl.mesander.pdf_to_excel.dtos.output.InvoiceDto;
+import nl.mesander.pdf_to_excel.models.Invoice;
 import nl.mesander.pdf_to_excel.repositories.InvoiceRepository;
+
+import java.util.List;
+
+import static nl.mesander.pdf_to_excel.helpers.CopyProperties.copyProperties;
 
 public class InvoiceService {
      private final InvoiceRepository invoiceRepository;
@@ -10,4 +17,26 @@ public class InvoiceService {
      }
 
      // Transfer Methods
+    public static Invoice dtoToInvoice(InvoiceInputDto inputDto) {
+         Invoice invoice = new Invoice();
+
+         copyProperties(inputDto, invoice);
+
+         return invoice;
+    }
+
+    public static InvoiceDto invoiceToDto(Invoice invoice) {
+         InvoiceDto invoiceDto = new InvoiceDto();
+
+         copyProperties(invoice, invoiceDto);
+
+         return invoiceDto;
+    }
+
+    // CRUD Methods
+    public List<InvoiceDto> getInvoices() {}
+    public InvoiceDto getInvoice() {}
+    public List<InvoiceDto> getInvoicesByFilter() {}
+    public InvoiceDto createInvoice() {}
+    public String deleteInvoice() {}
 }
