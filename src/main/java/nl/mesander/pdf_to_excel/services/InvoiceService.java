2fc8a8f556c435afae11b54203b6c69ced8b0bc3
Mark Mesander <mark_mesander@hotmail.com>
2025-01-08 20:02:03 +0100

    add create and delete invoice methods and trim code

--- src/main/java/nl/mesander/pdf_to_excel/services/InvoiceService.java
+++ src/main/java/nl/mesander/pdf_to_excel/services/InvoiceService.java
@@ -1,63 +1,90 @@
 package nl.mesander.pdf_to_excel.services;
 
+// Imports
 import nl.mesander.pdf_to_excel.dtos.input.InvoiceInputDto;
 import nl.mesander.pdf_to_excel.dtos.output.InvoiceDto;
+import nl.mesander.pdf_to_excel.exceptions.InvalidInputException;
 import nl.mesander.pdf_to_excel.exceptions.RecordNotFoundException;
 import nl.mesander.pdf_to_excel.models.Invoice;
 import nl.mesander.pdf_to_excel.repositories.InvoiceRepository;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import static nl.mesander.pdf_to_excel.helpers.CopyProperties.copyProperties;
 
 public class InvoiceService {
-     private final InvoiceRepository invoiceRepository;
+    private final InvoiceRepository invoiceRepository;
 
-     public InvoiceService(InvoiceRepository invoiceRepository) {
-         this.invoiceRepository = invoiceRepository;
-     }
+    public InvoiceService(InvoiceRepository invoiceRepository) {
+        this.invoiceRepository = invoiceRepository;
+    }
 
-     // Transfer Methods
+    // Transfer Methods
     public static Invoice dtoToInvoice(InvoiceInputDto inputDto) {
-         Invoice invoice = new Invoice();
+        Invoice invoice = new Invoice();
 
-         copyProperties(inputDto, invoice);
+        copyProperties(inputDto, invoice);
+        invoice.setInvoiceNumber(inputDto.getInvoiceNumber().toUpperCase());
 
-         return invoice;
+        return invoice;
     }
 
     public static InvoiceDto invoiceToDto(Invoice invoice) {
-         InvoiceDto invoiceDto = new InvoiceDto();
+        InvoiceDto invoiceDto = new InvoiceDto();
 
-         copyProperties(invoice, invoiceDto);
+        copyProperties(invoice, invoiceDto);
 
-         return invoiceDto;
+        return invoiceDto;
     }
 
     // CRUD Methods
     public List<InvoiceDto> getInvoices() {
-         List<Invoice> invoices = invoiceRepository.findAll();
-         List<InvoiceDto> invoiceDtos = new ArrayList<>();
+        List<Invoice> invoices = invoiceRepository.findAll();
+        List<InvoiceDto> invoiceDtos = new ArrayList<>();
 
-         for (Invoice invoice : invoices) {
-             InvoiceDto invoiceDto = invoiceToDto(invoice);
-             invoiceDtos.add(invoiceDto);
-         }
+        for (Invoice invoice : invoices) {
+            InvoiceDto invoiceDto = invoiceToDto(invoice);
+            invoiceDtos.add(invoiceDto);
+        }
 
-         return invoiceDtos;
+        if (invoiceDtos.isEmpty()) {
+            throw new RecordNotFoundException("No invoices found");
+        } else {
+            return invoiceDtos;
+        }
     }
 
     public InvoiceDto getInvoice(String invoiceNumber) {
-         String invoiceNumberUppercase = invoiceNumber.toUpperCase();
+        String invoiceNumberUppercase = invoiceNumber.toUpperCase();
 
-         Invoice invoice = invoiceRepository.findById(invoiceNumberUppercase)
-                 .orElseThrow(() -> new RecordNotFoundException(invoiceNumberUppercase));
+        Invoice invoice = invoiceRepository.findById(invoiceNumberUppercase)
+                .orElseThrow(() -> new RecordNotFoundException("Invoice with number: " + invoiceNumberUppercase + " not found"));
 
-         return invoiceToDto(invoice);
+        return invoiceToDto(invoice);
     }
 
-    public List<InvoiceDto> getInvoicesByFilter() {}
-    public InvoiceDto createInvoice() {}
-    public String deleteInvoice() {}
+//    public List<InvoiceDto> getInvoicesByFilter() {}
+
+    public InvoiceDto createInvoice(InvoiceInputDto inputDto) {
+        Invoice invoice = new Invoice();
+
+        boolean invoiceNumberExists = invoiceRepository.existsById(inputDto.getInvoiceNumber());
+
+        if (invoiceNumberExists) {
+            throw new InvalidInputException("Invoice with number: " + inputDto.getInvoiceNumber() + " already exists");
+        } else {
+            invoiceRepository.save(invoice);
+            return invoiceToDto(invoice);
+        }
+    }
+
+    public String deleteInvoice(String invoiceNumber) {
+        String invoiceNumberUppercase = invoiceNumber.toUpperCase();
+
+        Invoice invoice = invoiceRepository.findById(invoiceNumberUppercase)
+                .orElseThrow(() -> new RecordNotFoundException("Invoice with number: " + invoiceNumberUppercase + " not found"));
+
+        invoiceRepository.delete(invoice);
+
+        return "Invoice with number: " + invoiceNumberUppercase + " is deleted";
+    }
 }
