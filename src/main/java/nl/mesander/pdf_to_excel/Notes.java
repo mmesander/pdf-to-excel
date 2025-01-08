dcef66e35c3b92d244cd9b985080e76d9441c3b7
Mark Mesander <mark_mesander@hotmail.com>
2025-01-08 17:43:03 +0100

    add notes

---
+++ src/main/java/nl/mesander/pdf_to_excel/Notes.java
@@ -0,0 +1,11 @@
+package nl.mesander.pdf_to_excel;
+
+public class Notes {
+
+    // 1. 1 of meer PDF bestanden moeten aangeleverd kunnen worden
+    // 2. De tekst moet uit het PDF bestand gehaald worden
+    // 3. De tekst moet in een excel bestand gezet worden
+    // - Er moet gecheckt worden welke maand het is
+    // - Indien er nog geen bestand is aangemaakt voor de maand van de factuurdatum, moet dat bestand aangemaakt worden, en daar moet de data ingevoegd worden
+    // - Indien er wel een bestand is moet de data daaraan toegevoegd worden
+}
