5729f9dc310582f37cb5a6dbb2f0a31ed493db38
Mark Mesander <mark_mesander@hotmail.com>
2025-01-08 19:38:34 +0100

    add getInvoices method

--- src/main/java/nl/mesander/pdf_to_excel/services/InvoiceService.java
+++ src/main/java/nl/mesander/pdf_to_excel/services/InvoiceService.java
@@ -5,6 +5,7 @@ import nl.mesander.pdf_to_excel.dtos.output.InvoiceDto;
 import nl.mesander.pdf_to_excel.models.Invoice;
 import nl.mesander.pdf_to_excel.repositories.InvoiceRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static nl.mesander.pdf_to_excel.helpers.CopyProperties.copyProperties;
@@ -34,8 +35,27 @@ public class InvoiceService {
     }
 
     // CRUD Methods
-    public List<InvoiceDto> getInvoices() {}
-    public InvoiceDto getInvoice() {}
+    public List<InvoiceDto> getInvoices() {
+         List<Invoice> invoices = invoiceRepository.findAll();
+         List<InvoiceDto> invoiceDtos = new ArrayList<>();
+
+         for (Invoice invoice : invoices) {
+             InvoiceDto invoiceDto = invoiceToDto(invoice);
+             invoiceDtos.add(invoiceDto);
+         }
+
+         return invoiceDtos;
+    }
+
+    public InvoiceDto getInvoice(String invoiceNumber) {
+         String invoiceNumberUppercase = invoiceNumber.toUpperCase();
+    }
+
+
+
+
+
+
     public List<InvoiceDto> getInvoicesByFilter() {}
     public InvoiceDto createInvoice() {}
     public String deleteInvoice() {}
